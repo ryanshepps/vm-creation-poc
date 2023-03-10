b2cfd965c3051f0c45ac431697950e4e6e1d4909
ryanshepps <sheppardryan@rogers.com>
2023-03-10 13:10:40 -0500

    feat: add documentation from conf as labels

--- classes/apis/gcloud/classes/Instance.py
+++ classes/apis/gcloud/classes/Instance.py
@@ -3,6 +3,18 @@ from google.cloud import compute_v1
 
 class Instance:
 
+    def __create_labels(instance_configuration):
+        formatted_labels = {}
+
+        for key, label in instance_configuration.items():
+            formatted_labels[key] = label.replace(" ", "-").lower()
+
+        return {
+            "team": formatted_labels["team"],
+            "purpose": formatted_labels["purpose"],
+            "os": formatted_labels["os"]
+        }
+
     def __new__(cls, instance_configuration, disks, networks):
         instance = compute_v1.Instance()
         instance.disks = disks
@@ -10,5 +22,6 @@ class Instance:
         instance.name = instance_configuration["name"]
         instance.machine_type = \
             f'zones/{instance_configuration["zone"]}/machineTypes/f1-micro'
+        instance.labels = cls.__create_labels(instance_configuration)
 
         return instance
